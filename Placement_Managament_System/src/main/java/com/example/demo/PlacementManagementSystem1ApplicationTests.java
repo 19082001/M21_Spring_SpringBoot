fb2fe80e08b302128f259057bc5b2be19c608ece
DELL <DELL@DESKTOP-MJ6JMGF>
2022-09-20 10:15:36 +0530

    First Commit

---
+++ Placement_Managament_System/src/main/java/com/example/demo/PlacementManagementSystem1ApplicationTests.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class PlacementManagementSystem1ApplicationTests 
+{
+
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(PlacementManagementSystem1ApplicationTests.class, args);
+	}
+
+}
